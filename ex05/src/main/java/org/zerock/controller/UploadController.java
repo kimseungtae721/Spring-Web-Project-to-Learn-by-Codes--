fc732da06c4e28364f0bcdd43e21daf0711f993f
kst005109 <kst005109@naver.com>
2021-01-29 17:48:00 +0900

    첨부파일 다운로드, 원본보여주기, 첨부파일 삭제

--- ex05/src/main/java/org/zerock/controller/UploadController.java
+++ ex05/src/main/java/org/zerock/controller/UploadController.java
@@ -3,6 +3,9 @@ package org.zerock.controller;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.nio.file.Files;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -10,6 +13,8 @@ import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -19,8 +24,10 @@ import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.adapter.HttpWebHandlerAdapter;
 import org.zerock.domain.AttachFileDTO;
 
 import lombok.extern.log4j.Log4j;
@@ -37,7 +44,7 @@ public class UploadController {
 		
 	}
 	
-	//스프링 mvc에서 제공하는 MultipartFile 타입 사용해서 데이터받기 
+	//form 방식으로 파일업로드처리 
 	@PostMapping("/uploadFormAction")
 	public void uploadFormPost(MultipartFile[] uploadfile, Model model) {
 		
@@ -124,6 +131,8 @@ public class UploadController {
 		}//end for
 	}
 */
+	
+	//ajax방식 파일업로드 처리
 	@PostMapping(value = "/uploadAjaxAction", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	@ResponseBody
 	public ResponseEntity <List<AttachFileDTO>> uploadAjaxPost(MultipartFile[] uploadFile){
@@ -243,5 +252,85 @@ public class UploadController {
 		}
 		return result;
 	}
+
+	//파일 다운로드 처리
+	@GetMapping(value = "/download", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+	@ResponseBody
+	public ResponseEntity<Resource> downloadFile(@RequestHeader("User-Agent")String userAgent, String fileName){
+		
+		log.info("download file: " + fileName);
+		
+		Resource resource = new FileSystemResource("c:\\upload\\" + fileName);
+		
+		//값이 있는지 여부 확인
+		if(resource.exists() == false) {
+			log.info("resource 값이없음");
+			return new ResponseEntity<Resource>(HttpStatus.NOT_FOUND);
+		}
+		
+		//UUID 삭제
+		String resourceName = resource.getFilename();
+		String resourceOriginalName = resourceName.substring(resourceName.indexOf("_") + 1);
+		
+		log.info("resource : " + resource);
+		
+		HttpHeaders headers = new HttpHeaders();
+		
+		try {
+			String downloadName = "null";
+			
+			if(userAgent.contains("Trident")) {
+				log.info("IE browser");
+				downloadName = URLEncoder.encode(resourceOriginalName,"UTF-8").replaceAll("\\+"," ");
+				log.info("IE browser" + downloadName);
+				
+			}else if(userAgent.contains("Edge")) {
+				log.info("Edge browser");
+				downloadName = URLEncoder.encode(resourceOriginalName, "UTF-8");
+				log.info("Edge Name: " + downloadName);
+			
+			}else {
+				log.info("Chrome browser");
+				downloadName = new String (resourceOriginalName.getBytes("UTF-8"),"ISO-8859-1");
+			}
+			
+//			headers.add("Content-Disposition", "attachment; filename=" + downloadName);
+			headers.add("Content-Disposition", "attachment; filename=" + downloadName); 
+		}catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity<Resource>(resource, headers, HttpStatus.OK);
+	}
 	
+	//서버에서 첨부파일 삭제
+	@PostMapping("/deleteFile")
+	@ResponseBody
+	public ResponseEntity<String> deleteFile(String fileName, String type){
+		
+		log.info("deleteFile :" + fileName);
+		
+		File file;
+		
+		try {
+			file = new File("c:\\upload\\" + URLDecoder.decode(fileName, "UTF-8"));	
+			
+			file.delete();
+			
+			if(type.equals("image")) {
+				String largeFileName = file.getAbsolutePath().replace("s_", "");
+				
+				log.info("largeFileName: " + largeFileName);
+				
+				file = new File(largeFileName);
+				
+				file.delete();
+			}
+		}catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
+		}
+		
+		return new ResponseEntity<String>("deleted", HttpStatus.OK);
+		
+	}
 }
--- ex05/src/main/webapp/WEB-INF/views/uploadAjax.jsp
+++ ex05/src/main/webapp/WEB-INF/views/uploadAjax.jsp
@@ -6,6 +6,10 @@
 <meta charset="UTF-8">
 <title>Insert title here</title>
 </head>
+<div class="bigPictureWrapper">
+	<div class="bigPicture">
+	</div>
+</div>
 <style>
 .uploadResult {
 	width: 100%;
@@ -25,8 +29,33 @@
 }
 
 .uploadResult ul li img{
-	width: 200px;
+	width: 100px;
 }
+
+.uploadResult ul li span {
+	color: white;
+}
+
+.bigPictureWrapper {
+	position: absolute;
+	display: none;
+	justify-content: center;
+	align-items: center;
+	top: 0%;
+	width: 100%;
+	height: 100%;
+	background: rgba(255,255,255,0.5);
+}
+.bigPicture{
+	position: relative;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	}
+.bigPicture img {
+	width: 600px;	
+}
+
 </style>
 <body>
 
@@ -47,10 +76,21 @@
 	
 	</ul>
 </div>
-
-
 <script>
+
+//원본 이미지 띄워주기
+function showImage(fileCallPath) {
+	//alert(fileCallPath);
+
+	$(".bigPictureWrapper").css("display","flex").show();
+	
+	$(".bigPicture")
+	.html("<img src='/display?fileName="+encodeURI(fileCallPath)+"'>") 
+	.animate({width: '100%', height: '100%'}, 1000);
+}
+
 $(document).ready(function(){
+	
 	
 	var regex = new RegExp("(.*?)\.(exe|sh|zip|alz)$");
 	var maxSize = 5242880; //5MB
@@ -70,30 +110,54 @@ $(document).ready(function(){
 		return true;
 	}
 	
-	//<ul> 태그내에 파일 이름 출력
+	//<ul> 태그내에 파일 이름 출력(파일다운로드)
 	var uploadResult = $(".uploadResult ul");
 	
 	function showUploadedFile(uploadResultArr){
 		var str = "";		
 		
 		$(uploadResultArr).each(function(i, obj) {
-			
-			if(!obj.image){
-				str += "<li><img src='/resources/img/attach.png'>" + obj.fileName + "</li>";
-			}else{
-				
-				var fileCallPath = encodeURIComponent( obj.uploadPath + "/s_" + obj.uuid + "_" + obj.fileName);
-				
-				str += "<li><img src='/display?fileName="+fileCallPath+"'></li>";
-			}
 
+		if(!obj.image){
+			//	str += "<li><img src='/resources/img/attach.png'>" + obj.fileName + "</li>";
+			var fileCallPath = encodeURIComponent(obj.uploadPath+ "/" + obj.uuid + "_" + obj.fileName);
+			str += "<li><a href='/download?fileName="+fileCallPath+"'>" 
+					+ "<img src='/resources/img/attach.png'>"
+					+ obj.fileName + "</a></li>" 
+					+"<span data-file=\'"+fileCallPath+"\' data-type='file'> x </span>" + "<div></li>"
+			console.log(str);
+		}
+		else{
+			var fileCallPath = encodeURIComponent( obj.uploadPath + "/s_" + obj.uuid + "_" + obj.fileName);
 			
-		});
-		
+			var originPath = obj.uploadPath+ "\\" + obj.uuid + "_" + obj.fileName;
+			
+			originPath = originPath.replace(new RegExp(/\\/g),"/");
+			
+			str += "<li><a href=\"javascript:showImage(\'"+originPath+"\')\"><img src='/display?fileName="+fileCallPath+"'></a>" 
+					+"<span data-file=\'"+fileCallPath+"\' data-type='image'> x </span>" + "<li>";
+		}
+	});
 		uploadResult.append(str);
-	}
+}
 	
-
+	$(".uploadResult").on("click","span", function(e) {
+		
+		var targetFile = $(this).data("file");
+		var type = $(this).data("type");
+		console.log(targetFile);
+		
+		$.ajax({
+			url : "/deleteFile",
+			data : {fileName: targetFile, type: type},
+			dataType: "text",
+			type : "post",
+				success: function(result) {
+					alert(result);
+				}
+		}); //ajax end
+	});
+	
 	var cloneObj = $(".uploadDiv").clone();
 	
 	//uploadBtn 클릭시
@@ -117,7 +181,16 @@ $(document).ready(function(){
 		formData.append("uploadFile", files[i]);
 		
 	}
-
+	
+	// 원본이미지 다시 사라지도록 하는 이벤트
+	$(".bigPictureWrapper").on("click", function(e) {
+		
+		$(".bigPicture").animate({width:'0%', height: '0%'}, 1000);
+		setTimeout(function() {
+			$('.bigPictureWrapper').hide();
+		}, 1000);
+	});
+	
 	$.ajax({  
 		url: "/uploadAjaxAction",
 		processData: false,
@@ -133,7 +206,6 @@ $(document).ready(function(){
 			
 		} 
 	}); //$ajax
-
 	
 	});
 });
